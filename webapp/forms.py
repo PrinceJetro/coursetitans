58ffd55b2eb76b064f52f9557801f7278fe0c1ac
PrinceJetro <adegbuyijephthah@gmail.com>
2024-11-06 16:56:05 +0100

    urls and forms

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,14 +1,36 @@
-from django.forms import ModelForm
-from .models import *
+# forms.py
+from django import forms
+from django.contrib.auth.models import User
+from .models import  Course, Topic
+from ckeditor.fields import RichTextField  # For model definition
 
-
-class RoomForm(ModelForm):
+class UserRegistrationForm(forms.ModelForm):
+    username = forms.CharField(max_length=150, required=True)
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    email = forms.EmailField(required=True)
+    password = forms.CharField(widget=forms.PasswordInput, required=True)
+    confirm_password = forms.CharField(widget=forms.PasswordInput, label="Confirm Password", required=True)
+    
     class Meta:
-        model = Complaint
-        fields = '__all__'
-        exclude = ['host', 'participants', 'topic']
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password']
 
-class AvatarForm(ModelForm):
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password and confirm_password and password != confirm_password:
+            self.add_error('confirm_password', "Passwords do not match.")
+        return cleaned_data
+
+class TopicForm(forms.ModelForm):
+    name = forms.CharField(max_length=255, required=True)
+    course = forms.ModelChoiceField(queryset=Course.objects.all(), required=True)
+    description = forms.CharField(widget=forms.Textarea, required=True)
+    content = forms.CharField(widget=forms.Textarea, required=True)  # Ensuring content is required with a Textarea widget
+
     class Meta:
-        model  = User
-        fields =  ['avatar']
+        model = Topic
+        fields = ['name', 'course', 'description', 'content']
--- webapp/urls.py
+++ webapp/urls.py
@@ -1,9 +1,17 @@
-from .views import *
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
 
 urlpatterns = [
-    path('', getRoutes, name='register'),
-    path('all', getAll, name='all'),
-    path('create', createComplaint, name='create'),
-    path('student', createStudent, name='student'),
+    path('', views.home, name='home'),
+    path('register/', views.register_student, name='register'),
+    path('myprofile/', views.myprofile, name='myprofile'),
+    path('login/', views.loginview, name='login'),  # Login URL
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),  # Logout URL
+    path('departments/', views.list_departments, name='list_departments'),
+    path('departments/<str:department_name>/courses/', views.list_courses, name='list_courses'),
+    path('courses/', views.list_all_courses, name='course'),
+    path('courses/<str:course_name>/', views.course_detail, name='course_detail'),
+    path('courses/<str:topic_name>/topic', views.topic_detail, name='topic_detail'),
 ]

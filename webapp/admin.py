90287e092ede272937470391ba08140b11473842
PrinceJetro <adegbuyijephthah@gmail.com>
2024-11-06 16:51:20 +0100

    admin

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,7 +1,30 @@
+# admin.py
 from django.contrib import admin
-from .models  import *
-# Register your models here.
+from .models import Student, Course, Department, Topic
 
-admin.site.register(Complaint)
-admin.site.register(User)
-admin.site.register(OnlineClass)
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['user']
+    search_fields = ('user__username', 'user__first_name', 'user__last_name', 'tutorial_center__name')
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_departments')  # Use custom method for departments
+    search_fields = ('name', 'departments__name')
+    list_filter = ('departments',)  # Filter by the `departments` field
+
+    def get_departments(self, obj):
+        return ", ".join([dept.name for dept in obj.departments.all()])  # Display departments as a comma-separated list
+
+    get_departments.short_description = 'Departments'  # Set column name for departments
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name', 'course')
+    search_fields = ('name', 'course__name')
+    list_filter = ('course',)
